e65bd4ccf6bc06d8ef8255b80fa91244ea95fe31
Bastian de Groot <git@de-groot.info>
2018-03-15 18:32:11 +0100

    add title matching

--- youtube-download.sh
+++ youtube-download.sh
@@ -9,6 +9,9 @@ DATEAFTER="now-1month"
 FORMAT="bestvideo+bestaudio"
 THREADS=5
 
+
+IFS=$'\n'
+
 channeldownload () {
     "$YOUTUBEDL" --playlist-end "$MAXDOWNLOAD" \
                  --download-archive "$DOWNLOADDIRECTORY/archive-list" \
@@ -17,6 +20,7 @@ channeldownload () {
                  --no-overwrites \
                  --ignore-errors \
                  --format "$FORMAT" \
+                 --match-title "$2" \
                  --output "$DOWNLOADDIRECTORY/%(uploader)s/%(upload_date)s-%(title)s-%(id)s.%(ext)s" "$1"
 }
 
@@ -25,11 +29,13 @@ if [ ! -f "$CHANNELLIST" ] ; then
     exit 1
 fi
 
-for CHANNEL in $(cat $CHANNELLIST) ; do
+for line in $(grep -v "^#" "$CHANNELLIST") ; do
     while [ "$(jobs | wc -l)" -ge "$THREADS" ] ; do
         sleep 1
     done
-    channeldownload "$CHANNEL" &
+    url="$(echo "$line" | cut -d " " -f1)"
+    pattern="$(echo "$line" | grep " " | cut -d " " -f2-)"
+    channeldownload "$url" "$pattern" &
     sleep "0.1"
 done
 
