4391e218f6996c01b689a31e5db82585482459f8
Bastian de Groot <git@de-groot.info>
2020-12-18 10:56:54 +0100

    format podcast download; workaround for shitty rss file naming

--- podcast-download.sh
+++ podcast-download.sh
@@ -9,39 +9,51 @@ DATEAFTER="now-1month"
 FORMAT="bestaudio"
 THREADS=5
 
-
 IFS=$'\n'
 
-download () {
-    "$YOUTUBEDL" --playlist-end "$MAXDOWNLOAD" \
-                 --download-archive "$DOWNLOADDIRECTORY/archive-list" \
-                 --dateafter "$DATEAFTER" \
-                 --continue \
-                 --no-overwrites \
-                 --ignore-errors \
-                 --format "$FORMAT" \
-                 --match-title "$2" \
-                 --output "$3/%(upload_date)s-%(title)s-%(id)s.%(ext)s" "$1"
+download() {
+  if [[ "$4" == "true" ]]; then
+    ARCHIVE_PARAM=()
+  else
+    ARCHIVE_PARAM=(--download-archive "$DOWNLOADDIRECTORY/archive-list")
+  fi
+  "$YOUTUBEDL" --playlist-end "$MAXDOWNLOAD" \
+    --dateafter "$DATEAFTER" \
+    --continue \
+    --no-overwrites \
+    --ignore-errors \
+    "${ARCHIVE_PARAM[@]}" \
+    --format "$FORMAT" \
+    --match-title "$2" \
+    --output "$3/%(upload_date)s-%(title)s-%(id)s.%(ext)s" "$1"
 }
 
-title () {
-    curl -s -L "$1" | awk -vRS="</title>" '/<title>/{gsub(/.*<title>|\n+/,"");sub(/%/,"Percent");print;exit}'
+title() {
+  curl -s -L "$1" | awk -vRS="</title>" '/<title>/{gsub(/.*<title>|\n+/,"");gsub(/%/,"Percent");gsub(/<!\[CDATA\[|>|]/,"");print;exit}'
 }
 
-if [ ! -f "$CHANNELLIST" ] ; then
-    echo "channel-list missing" >&2
-    exit 1
+if [ ! -f "$CHANNELLIST" ]; then
+  echo "channel-list missing" >&2
+  exit 1
 fi
 
-for line in $(grep -v "^#" "$CHANNELLIST") ; do
-    while [ "$(jobs | wc -l)" -ge "$THREADS" ] ; do
-        sleep 1
-    done
-    url="$(echo "$line" | cut -d " " -f1)"
-    pattern="$(echo "$line" | grep " " | cut -d " " -f2-)"
-    
-    download "$url" "$pattern" "$DOWNLOADDIRECTORY/$(title "$url")" &
-    sleep "0.1"
+for line in $(grep -v "^#" "$CHANNELLIST"); do
+  while [ "$(jobs | wc -l)" -ge "$THREADS" ]; do
+    sleep 1
+  done
+  url="$(echo "$line" | cut -d " " -f1)"
+  pattern="$(echo "$line" | grep " " | cut -d " " -f2-)"
+
+  # Workaround for a podcast always using the same file name
+  # TODO make this configurable
+  if grep -q "9YNI3WaL" <<<"$url"; then
+    NODOWNLOADARCHIVE=true
+  else
+    NODOWNLOADARCHIVE=false
+  fi
+
+  download "$url" "$pattern" "$DOWNLOADDIRECTORY/$(title "$url")" "$NODOWNLOADARCHIVE" &
+  sleep "0.1"
 done
 
 wait
